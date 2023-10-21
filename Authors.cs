fd53ab82a14f1e2c2fa4c692243cb1c0eb3cc597
Ana <Ana@HP250NOTEBOOK>
2023-10-21 18:40:40 +0300

    Add project files.

---
+++ Authors.cs
@@ -0,0 +1,9 @@
+﻿namespace Legian_Ana_Lab2
+{
+    public class Authors
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Data/Legian_Ana_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Legian_Ana_Lab2.Models;
+
+namespace Legian_Ana_Lab2.Data
+{
+    public class Legian_Ana_Lab2Context : DbContext
+    {
+        public Legian_Ana_Lab2Context (DbContextOptions<Legian_Ana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Legian_Ana_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Legian_Ana_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
